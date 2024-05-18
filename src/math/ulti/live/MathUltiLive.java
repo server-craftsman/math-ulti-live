5f1957631fba78799d40991f3d31bb11c6aa486b
server-craftsman <huyit2003@gmail.com>
2024-05-18 13:11:43 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/math/ulti/live/MathUltiLive.java
+++ src/math/ulti/live/MathUltiLive.java
@@ -16,11 +16,16 @@ public class MathUltiLive {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ngu");
+        
+        System.out.println("Tinh giai thua");
         long result = MathUlti.getFactorial(5); //hàm tính đi coi là mấy
         System.out.println("expected: 5! = 120; actual: " + result);
         System.out.println("expected: 6! = 720; actual: " + MathUlti.getFactorial(6));
-        System.out.println("expected: 0! = 1; actual: " + MathUlti.getFactorial(1));
+        System.out.println("expected: 0! = 1; actual: " + MathUlti.getFactorial(0));
+        
+        //Thêm code sau lần đầu tiên làm chuyện ấy - code lên server 1:09PM 18/5/2024
+        System.out.println("expected: 1! = 1; actual: " + MathUlti.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUlti.getFactorial(3));
     }
     
 }
